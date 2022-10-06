c448342ebf23adceb33a52287859b8f02e919530
morgan_p <morgan.prieur@tetradis.com>
2022-10-06 19:00:52 +0200

    Loop for auto. the Players instanciations: ok. TODO: auto serializations

--- project.py
+++ project.py
@@ -21,41 +21,72 @@ players_table = db.table('players')
 # Make a list of the players data 
 
 players_data = prompt_for_players_data() 
+print('\n') 
+print('players_data : ', players_data) 
+print('\n') 
+print('players_data[0] : ', players_data[0]) 
 # players_data_list = player_data # marche pas, il faut plusieurs jeux de data 
 
-if(len(players_data)) == 1: 
+players = [] 
 
-    # Reçus de la réponse au prompt : 
-    player1 = player_model.Player(
-        lastname = players_data[0]['ask_for_lastname'], 
-        firstname = players_data[0]['ask_for_firstname'], 
-        birthdate = players_data[0]['ask_for_birthdate'], 
-        genre = players_data[0]['ask_for_genre'], 
-        classement = players_data[0]['ask_for_classement'] 
+for x in range(len(players_data)): 
+    print(f'x : {x}\n') 
+    player_x = player_model.Player(
+        lastname = players_data[x]['ask_for_lastname'], 
+        firstname = players_data[x]['ask_for_firstname'], 
+        birthdate = players_data[x]['ask_for_birthdate'], 
+        genre = players_data[x]['ask_for_genre'], 
+        classement = players_data[x]['ask_for_classement']
     ) 
-    print(player1) 
+    print('player_x : ', player_x) 
+    players.append(player_x) 
+    print(f'players[{x}] : ', players[x]) 
 
-elif len(players_data) == 2: 
-        # Reçus de la réponse au prompt : 
-    player1 = player_model.Player(
-        lastname = players_data[0]['ask_for_lastname'], 
-        firstname = players_data[0]['ask_for_firstname'], 
-        birthdate = players_data[0]['ask_for_birthdate'], 
-        genre = players_data[0]['ask_for_genre'], 
-        classement = players_data[0]['ask_for_classement'] 
-    )
-    player2 = player_model.Player(
-        lastname = players_data[1]['ask_for_lastname'], 
-        firstname = players_data[1]['ask_for_firstname'], 
-        birthdate = players_data[1]['ask_for_birthdate'], 
-        genre = players_data[1]['ask_for_genre'], 
-        classement = players_data[1]['ask_for_classement'] 
-    ) 
-    print('\n') 
-    print(player1) 
-    print('\n') 
-    print(player2) 
-    print('\n') 
+    count_players = 0 
+    count_players += 1 if isinstance(player_x, player_model.Player) else count_players 
+
+    if count_players > 1: 
+        print(f'There is {count_players} subscribers to the chess tournament') 
+    else: 
+        print(f'There is {count_players} subscriber to the chess tournament') 
+
+
+print('\n') 
+print('players : ', players) 
+
+# if(len(players_data)) == 1: 
+
+#     # Reçus de la réponse au prompt : 
+#     player1 = player_model.Player(
+#         lastname = players_data[0]['ask_for_lastname'], 
+#         firstname = players_data[0]['ask_for_firstname'], 
+#         birthdate = players_data[0]['ask_for_birthdate'], 
+#         genre = players_data[0]['ask_for_genre'], 
+#         classement = players_data[0]['ask_for_classement'] 
+#     ) 
+#     print(player1) 
+
+# elif len(players_data) == 2: 
+#         # Reçus de la réponse au prompt : 
+#     player1 = player_model.Player(
+#         lastname = players_data[0]['ask_for_lastname'], 
+#         firstname = players_data[0]['ask_for_firstname'], 
+#         birthdate = players_data[0]['ask_for_birthdate'], 
+#         genre = players_data[0]['ask_for_genre'], 
+#         classement = players_data[0]['ask_for_classement'] 
+#     )
+#     player2 = player_model.Player(
+#         lastname = players_data[1]['ask_for_lastname'], 
+#         firstname = players_data[1]['ask_for_firstname'], 
+#         birthdate = players_data[1]['ask_for_birthdate'], 
+#         genre = players_data[1]['ask_for_genre'], 
+#         classement = players_data[1]['ask_for_classement'] 
+#     ) 
+#     print('\n') 
+#     print(player1) 
+#     print('\n') 
+#     print(player2) 
+#     print('\n') 
 
 # oren = player_model.Player(
 #     'Prieur', 'Oren', '2009-03-25', 'garçon', 70
@@ -64,38 +95,22 @@ elif len(players_data) == 2:
 #     'Prieur', 'Chris', '1974-01-22', 'garçon', 68
 # ) 
 
-count_players = 0 
-count_players += 1 if isinstance(player2, player_model.Player) else count_players 
 
-if count_players > 1: 
-    print(f'There is {count_players} subscribers to the chess tournament') 
-else: 
-    print(f'There is {count_players} subscriber to the chess tournament') 
 
 player1 = serialize_player(
-        player1.lastname, 
-        player1.firstname, 
-        player1.birthdate, 
-        player1.genre, 
-        player1.classement 
-    )
-player2 = serialize_player(
-    player2.lastname, 
-    player2.firstname, 
-    player2.birthdate, 
-    player2.genre, 
-    player2.classement 
+    players[0].lastname, 
+    players[0].firstname, 
+    players[0].birthdate, 
+    players[0].genre, 
+    players[0].classement 
 ) 
-# player1 = serialize_player(
-#     player1.lastname, 
-#     player1.firstname, 
-#     player1.birthdate, 
-#     player1.genre, 
-#     player1.classement
-# ) 
-# chris = serialize_player(
-#     chris.lastname, chris.firstname, chris.birthdate, chris.genre, chris.classement
-# ) 
+player2 = serialize_player(
+    players[1].lastname, 
+    players[1].firstname, 
+    players[1].birthdate, 
+    players[1].genre, 
+    players[1].classement 
+) 
 
 # For charging some players in the DB : 
 serialized_players = [ 
