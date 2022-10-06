e3283c4892758b3c651a3dac41c9730311c4de09
morgan_p <morgan.prieur@tetradis.com>
2022-10-06 12:17:20 +0200

    Add many pl->db: ok. TODO: loop for auto. the instanciations

--- project.py
+++ project.py
@@ -1,7 +1,7 @@
 from cmd import PROMPT
 from Models import player_model 
 # functions 
-from utils.functions import prompt_for_one_player, serialize_player 
+from utils.functions import prompt_for_players_data, serialize_player 
 
 # TinyDB 
 from tinydb import TinyDB 
@@ -20,18 +20,43 @@ players_table = db.table('players')
 # stocké dans cette liste : 
 # Make a list of the players data 
 
-player_data = prompt_for_one_player() 
+players_data = prompt_for_players_data() 
 # players_data_list = player_data # marche pas, il faut plusieurs jeux de data 
 
+if(len(players_data)) == 1: 
 
-# Reçus de la réponse au prompt : 
-player1 = player_model.Player(
-    lastname = player_data['ask_for_lastname'], 
-    firstname = player_data['ask_for_firstname'], 
-    birthdate = player_data['ask_for_birthdate'], 
-    genre = player_data['ask_for_genre'], 
-    classement = player_data['ask_for_classement'] 
-) 
+    # Reçus de la réponse au prompt : 
+    player1 = player_model.Player(
+        lastname = players_data[0]['ask_for_lastname'], 
+        firstname = players_data[0]['ask_for_firstname'], 
+        birthdate = players_data[0]['ask_for_birthdate'], 
+        genre = players_data[0]['ask_for_genre'], 
+        classement = players_data[0]['ask_for_classement'] 
+    ) 
+    print(player1) 
+
+elif len(players_data) == 2: 
+        # Reçus de la réponse au prompt : 
+    player1 = player_model.Player(
+        lastname = players_data[0]['ask_for_lastname'], 
+        firstname = players_data[0]['ask_for_firstname'], 
+        birthdate = players_data[0]['ask_for_birthdate'], 
+        genre = players_data[0]['ask_for_genre'], 
+        classement = players_data[0]['ask_for_classement'] 
+    )
+    player2 = player_model.Player(
+        lastname = players_data[1]['ask_for_lastname'], 
+        firstname = players_data[1]['ask_for_firstname'], 
+        birthdate = players_data[1]['ask_for_birthdate'], 
+        genre = players_data[1]['ask_for_genre'], 
+        classement = players_data[1]['ask_for_classement'] 
+    ) 
+    print('\n') 
+    print(player1) 
+    print('\n') 
+    print(player2) 
+    print('\n') 
+
 # oren = player_model.Player(
 #     'Prieur', 'Oren', '2009-03-25', 'garçon', 70
 # ) 
@@ -39,39 +64,49 @@ player1 = player_model.Player(
 #     'Prieur', 'Chris', '1974-01-22', 'garçon', 68
 # ) 
 
-print(player1) 
-
 count_players = 0 
-count_players += 1 if isinstance(player1, player_model.Player) else count_players 
+count_players += 1 if isinstance(player2, player_model.Player) else count_players 
 
 if count_players > 1: 
     print(f'There is {count_players} subscribers to the chess tournament') 
 else: 
     print(f'There is {count_players} subscriber to the chess tournament') 
 
-
 player1 = serialize_player(
-    player1.lastname, 
-    player1.firstname, 
-    player1.birthdate, 
-    player1.genre, 
-    player1.classement
+        player1.lastname, 
+        player1.firstname, 
+        player1.birthdate, 
+        player1.genre, 
+        player1.classement 
+    )
+player2 = serialize_player(
+    player2.lastname, 
+    player2.firstname, 
+    player2.birthdate, 
+    player2.genre, 
+    player2.classement 
 ) 
+# player1 = serialize_player(
+#     player1.lastname, 
+#     player1.firstname, 
+#     player1.birthdate, 
+#     player1.genre, 
+#     player1.classement
+# ) 
 # chris = serialize_player(
 #     chris.lastname, chris.firstname, chris.birthdate, chris.genre, chris.classement
 # ) 
 
-
 # For charging some players in the DB : 
-# serialized_players = [ 
-#     oren, chris
-# ] 
+serialized_players = [ 
+    player1, player2 
+] 
 
 
 # Charger les joueurs sérialisés dans la bdd : 
 players_table.truncate() 
-players_table.insert(player1) 
-# players_table.insert_multiple(serialized_players) 
+# players_table.insert(player1) 
+players_table.insert_multiple(serialized_players) 
 
 
 
--- utils/functions.py
+++ utils/functions.py
@@ -3,60 +3,86 @@ from prompt_toolkit import PromptSession
 session = PromptSession() 
 
 # Doctest 
-def to_absolute(number): 
-    """
-    >>> to_absolute(3)
-    3
-    >>> to_absolute(-4)
-    4
-    """
-    if number < 0: 
-        return -number 
-    else: 
-        return number 
+# def to_absolute(number): 
+#     """
+#     >>> to_absolute(3)
+#     3
+#     >>> to_absolute(-4)
+#     4
+#     """
+#     if number < 0: 
+#         return -number 
+#     else: 
+#         return number 
 
 
 
 # Prompt to ask the attributes of one player 
-def prompt_for_one_player(): 
+def prompt_for_players_data(): 
     # print(f'Lastname : {ask_for_lastname}') 
     # print(f'Firstname : {ask_for_firstname}') 
     # print(f'Birthdate : {ask_for_birthdate}') 
     # print(f'Genre : {ask_for_genre}') 
     # print(f'Classement : {ask_for_classement}') 
-    player_data = {
-        'ask_for_lastname': session.prompt(
-            'Enter the player lastname: \n'
-        ), 
-        'ask_for_firstname': session.prompt(
-            'Enter the player firstname: \n'
-        ), 
-        'ask_for_birthdate': session.prompt(
-            'Enter the player birthdate: \n'
-        ), 
-        'ask_for_genre': session.prompt(
-            'Enter the player genre: \n'
-        ), 
-        'ask_for_classement': session.prompt(
-            'Enter the player classement: \n'
-        ), 
-        'ask_for_new_player': session.prompt(
-            'Do you want to register another new player ? (Y maj / N maj) \n'
-        ) 
-    } 
+    players_data = [] 
+
+    ask_for_number_of_players = session.prompt(
+        'How many players left ? \n'
+    ) 
+
+    print('\nask_for_number_of_players (ln33) : ', ask_for_number_of_players) 
+
+    nb_players_registered = 0 
+    players_to_register = int(ask_for_number_of_players) 
+
+    print('\nplayers_to_register (ln38) : ', players_to_register) 
+
+    while nb_players_registered <= players_to_register: 
+
+        # print('\players_to_register (ln42) : ', players_to_register) 
+
+        player_data = { 
+            'ask_for_lastname': session.prompt(
+                'Enter the player lastname: \n'
+            ), 
+            'ask_for_firstname': session.prompt(
+                'Enter the player firstname: \n'
+            ), 
+            'ask_for_birthdate': session.prompt(
+                'Enter the player birthdate: \n'
+            ), 
+            'ask_for_genre': session.prompt(
+                'Enter the player genre: \n'
+            ), 
+            'ask_for_classement': session.prompt(
+                'Enter the player classement: \n'
+            ) 
+        } 
+
+        players_data.append(player_data) 
+        print('\nplayers_to_register (ln61) : ', players_to_register) 
+        nb_players_registered += 1 
+        players_to_register -= 1 
+
+        print('\nnb_players_registered : ',  nb_players_registered) 
+        print('\nplayers_to_register (ln66) : ', players_to_register) 
+        
+
+            # 'ask_for_new_player': session.prompt(
+            #     'Do you want to register another new player ? (Y maj / N maj) \n'
+            # ) 
 
 
-    # Condition if another new player or not 
-    if player_data['ask_for_new_player'] == 'Y': 
-        # other_player = prompt_for_one_player() 
-        print('another player') 
-    elif player_data['ask_for_new_player'] == 'N': 
-        print('done') 
-    else: 
-        print('Unknown command') 
+    # # Condition if another new player or not 
+    # if player_data['ask_for_new_player'] == 'Y': 
+    #     # other_player = prompt_for_one_player() 
+    #     print('another player') 
+    # elif player_data['ask_for_new_player'] == 'N': 
+    #     print('done') 
+    # else: 
+    #     print('Unknown command') 
 
-
-    return player_data 
+    return players_data 
 
 
 
